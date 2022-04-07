7df634ae17f804f7d2eedd72d236a1299abcb304
Dax2976 <ferrodavide55@gmail.com>
2022-04-07 15:01:57 +0200

    millestone 1 completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,52 @@
 // nome, ruolo e la stringa della foto
 
 
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+console.log(team)
+
+for(let i = 0; i < team.length; i++ ){
+    const person = team[i]
+
+    //console.log(typeof person)
+    console.log(person.name)
+    console.log(person.role)
+    console.log(person.image)
+    console.log(' ')
+}
+
+
+
+
+//millestone 1
