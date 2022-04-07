e5d04b14ecfefcf03e9051045282adc90950321e
Dax2976 <ferrodavide55@gmail.com>
2022-04-07 16:28:24 +0200

    millestone 2 + bonus 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,4 +65,75 @@ for(let i = 0; i < team.length; i++ ){
 
 
 
+
+
 //millestone 1
+
+
+
+//millestone 2 + bonus 1
+
+
+//stampare le stesse informazioni su DOM sottoforma di stringhe
+document.getElementById('cards')
+document.getElementById('container')
+const mainCard = document.getElementById('cards')
+
+
+let teams = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+
+for(let i = 0; i < teams.length; i++ ){
+    const membercard = document.createElement('div') 
+    const person = teams[i]
+    membercard.innerHTML = `<img src="img/${person.image}" alt="">
+    <h2>${person.name}</h2>
+    <h6>${person.role}</h6>`
+    mainCard.append(membercard)
+    membercard.classList.add('card')
+    mainCard.classList.add('mt-5','text-center')
+
+
+    //console.log(typeof person)
+    console.log(person.name)
+    console.log(person.role)
+    console.log(person.image)
+    console.log(' ')
+
+    
+}
+
+
+    
+
